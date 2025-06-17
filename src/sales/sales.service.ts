faff00d5ff9a3b6b33461022da4c490482f700d3
JavierLaravel <developer.laravel@sandigowebs.com>
2025-06-16 19:45:04 -0500

    Update sales.service.ts

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -3,12 +3,12 @@ import { CreateSaleDto } from './dto/create-sale.dto';
 import { UpdateSaleDto } from './dto/update-sale.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
-import { format, addDays  } from 'date-fns';
+import { format, addDays } from 'date-fns';
 
 
 @Injectable()
 export class SalesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async create(dto: CreateSaleDto): Promise<{ id: number; wasCreated: boolean }> {
     return await this.prisma.$transaction(async (tx) => {
@@ -16,22 +16,38 @@ export class SalesService {
         where: { observacion: dto.observacion },
         select: { id: true },
       });
-  
+
       if (existing) {
         return { id: Number(existing.id), wasCreated: false };
       }
-  
+
       const month = format(new Date(), 'MM');
       const serial = `1${month}`;
-  
+
+      /* const lastSale = await tx.sales.findFirst({
+         where: { serial },
+         orderBy: { comprobante: 'desc' },
+         select: { comprobante: true },
+       });*/
+
+      const now = new Date();
+      const startOfYear = new Date(now.getFullYear(), 0, 1); // 1 de enero
+      const startOfNextYear = new Date(now.getFullYear() + 1, 0, 1); // 1 de enero del siguiente año
+
       const lastSale = await tx.sales.findFirst({
-        where: { serial },
+        where: {
+          serial,
+          created_at: {
+            gte: startOfYear,
+            lt: startOfNextYear,
+          },
+        },
         orderBy: { comprobante: 'desc' },
         select: { comprobante: true },
       });
-  
+
       const comprobante = (lastSale?.comprobante ?? 0) + 1;
-  
+
       const sale = await tx.sales.create({
         data: {
           serial: serial,
@@ -52,9 +68,9 @@ export class SalesService {
           username_digita: 'WEB ALEA',
         },
       });
-  
+
       return { id: Number(sale.id), wasCreated: true };
     });
   }
-  
+
 }
